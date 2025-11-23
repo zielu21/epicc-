3671851a5439c3ac795289c747a1663a4f51074d
zielu21 <zieleniec21@gmail.com>
2025-11-23 10:17:24 +0100

    Starter Content

---
+++ Source/MojProjekt/MojProjekt.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MojProjekt.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MojProjekt, "MojProjekt" );
